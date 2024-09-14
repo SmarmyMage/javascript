94d998ff2ffc44d9fdd11674ec45f60c7735f50c
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-14 16:15:14 -0500

    layout update

---
+++ css/layout.css
@@ -0,0 +1,20 @@
+@media screen and (max-width: 499px) {
+    #homeImg {
+        height: 250px;
+        width: 350px;
+    }
+}
+
+@media screen and (min-width: 500px) {
+    #homeImg {
+        height: 300px;
+        width: 475px;
+    }
+}
+
+@media screen and (min-width: 100px) {
+    #homeImg {
+        width: 1900px;
+        height: 900px;
+    }
+}
