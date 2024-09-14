dbaa62ad6e0a33bf0a4e8bed0ba2512d0a47104f
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-09-14 14:53:05 -0500

    lesson 3 - home page

---
+++ javascript/javascript.js
@@ -0,0 +1,5 @@
+let visitorName = prompt("Welcome to my site!\nPlease tell use your name. ");
+
+window.alert("Hello, " + visitorName + "!");
+
+document.getElementById('pageWelcome').innerHTML = "Welcome " + visitorName + "! How are you today?";
