64f9f8d532d7e8e7c31c9dae0f1ea08d385a0fe9
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-10-04 22:46:06 -0500

    quiz alt implenentaion test

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,20 +1,24 @@
 var triviaQuest = ["the smallest irregular prime?", "the smallest perfect number?", "the second aria in the opera, Tosca?"];
 var triviaAns = ['37', '6', "E lucevan le stelle"];
 
-function triviaQuiz() {
 
-    var points = 0;
-    for (i = 0; i <= 3; i++) {    
-        var attempts = 3;
-        while (attempts > 0) {
-            ans = prompt("What is " + triviaQuest[0]);
-            if (ans == triviaAns[0]) {
-                alert("Correct!");
-                attempts = 0;
-            } else {
-                alert("Incorrect");
-                attempts = attempts - 1;
-            }
+var points = 0;
+for (i = 0; i <= 3; i++) {
+    var score = triviaQuiz(i);
+ }
+
+function triviaQuiz(i) {
+
+    var attempts = 3;
+    while (attempts > 0) {
+        ans = prompt("What is " + triviaQuest[i]);
+        if (ans == triviaAns[i]) {
+            alert("Correct!");
+            attempts = 0;
+        } else {
+            alert("Incorrect");
+            attempts = attempts - 1;
+        }
 
         if (attempts == 3) {
             points = points + 3;
@@ -25,8 +29,6 @@ function triviaQuiz() {
         } else {
             points = points + 0;
             attempts = 0;
-        }
-        
         }
     }
   return points;
