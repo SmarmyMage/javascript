2c666ceddcb895fa618893e2f4c057daa70aaf0c
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-10-04 21:04:51 -0500

    quiz test 5.7

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -4,7 +4,7 @@ var triviaAns = ['37', '6', "E lucevan le stelle"];
 function triviaQuiz() {
 
     var points = 0;
-    for (i = 0; i <= 3; i++) {    
+    for (var i = 0; i <= 3; i++) {    
         var attempts = 3;
         while (attempts > 0) {
             ans = prompt("What is " + triviaQuest[i]);
