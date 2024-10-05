b4e2a21408f76fa5a3e786e0f7b3ab0c8f3c18de
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-10-04 20:32:50 -0500

    quiz 5.1 test

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,33 +1,32 @@
 var triviaQuest = ["the smallest irregular prime?", "the smallest perfect number?", "the second aria in the opera, Tosca?"];
 var triviaAns = ['37', '6', "E lucevan le stelle"];
 
-var points = 0;
-for (i = 0; i <= 3; i++) {
-	var score = triviaQuiz(i);
-}
+function triviaQuiz() {
 
-function triviaQuiz(i) {
-    var attempts = 3;
-    while (attempts > 0) {
-      ans = prompt("What is " + triviaQuest[i]);
-      if (ans == triviaAns[i]) {
-        alert("Correct!");
-        attempts = 0;
-      } else {
-        alert("Incorrect");
-        attempts = attempts - 1;
-      }
+    var points = 0;
+    for (i = 0; i <= 3; i++) {    
+        var attempts = 3;
+        while (attempts > 0) {
+            ans = prompt("What is " + triviaQuest[0]);
+            if (ans == triviaAns[0]) {
+                alert("Correct!");
+                attempts = 0;
+            } else {
+                alert("Incorrect");
+                attempts = attempts - 1;
+            }
 
-      if (attempts == 3) {
-        points = points + 3;
-      } else if (attempts == 2) {
-        points = points + 2;
-      } else if (attempts == 1) {
-        points = points + 1;
-      } else {
-        points = points + 0;
-      }
-
+        if (attempts == 3) {
+            points = points + 3;
+        } else if (attempts == 2) {
+            points = points + 2;
+        } else if (attempts == 1) {
+            points = points + 1;
+        } else {
+            points = points + 0;
+        }
+        
+        }
     }
   return points;
 }
