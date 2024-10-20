30b4fe50a69554be4a570bfe0b1910b8dd2311ba
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-10-20 14:31:13 -0500

    trivia update 5

--- javascript/trivia.js
+++ javascript/trivia.js
@@ -2,19 +2,20 @@ var today = new Date();
 //var months = today.GetMonth();
 //var days = today.GetDay();
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var days = ['Sunday', 'Monday', "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+var days = ['Sunday', 'Monday', "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
 var date = today.getDate();
 var year = today.getFullYear();
 var hours = today.getHours();
 var minutes = today.getMinutes();
 
-function visitorName(userName) { 
+function name(userName) { 
     var firstLetter = userName.charAt(0);
     firstLetter = userName.toUpperCase();
 }
 
-let visitorName = prompt("Welcome!\nPlease tell use your name. ");
+let name = prompt("Welcome!\nPlease tell use your name. ");
 
+let visitorEmail = prompt("Please enter your email. ");
  
 function validEmail(email) {
     var emailRegex = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/;
@@ -24,11 +25,9 @@ function validEmail(email) {
         valid = valid[0].toUpperCase;
         return valid;
     } else {
-        document.write("Please enter a valid email. ");
+        window.alert("Please enter a valid email. ");
     }
 }
-
-let validEmail = prompt("Please enter your email. ");
 
 window.alert(validEmail([0]) + " + " + validEmail([1]));
 
@@ -51,51 +50,45 @@ var randQuote = quotes[Math.floor(Math.random()*3)+1];
 
 switch (hours) {
     case (hours < 12): {
-        window.alert("Good morning, " + visitorName + ".");
+        window.alert('Good morning, ' + visitorName + ".");
     }
     case (hours < 18): {
-        window.alert("Good afternoon, " + visitorName + ".");
+        window.alert('Good afternoon, ' + visitorName + ".");
     }
     default:
-        window.alert("Good evening, " + visitorName + ".");
+        window.alert('Good evening, ' + visitorName + ".");
 }
 
 var triviaQuest = ["the name of the dog that found the Jules rimet Cup in 1966?", "the name of the Soviet ghost town in Norway?", "the second aria in the opera, Tosca?"];
 var triviaAns = ["pickles", "pryamiden", "e lucevan le stelle"];
 
-var points = 0;
-var triviaQuest = triviaQuest.length
-for (var i = 0; i <= triviaQuest.length; i++) {
-    var score = triviaQuiz(i);
- }
-
 function triviaQuiz(i) {
-
-    var attempts = 2;
-    while (attempts > 0) {
-        ans = prompt("What is " + triviaQuest[i]);
-        ans = string.toLowerCase();
-        if (ans == triviaAns[i]) {
-            alert("Correct!");
-            attempts = 0;
-        } else {
-            alert("Incorrect");
-            attempts = attempts - 1;
+    var points = 0;
+    for (var i = 0; i <= triviaQuest.length; i++) {
+        var attempts = 2;
+        while (attempts > 0) {
+            ans = prompt("What is " + triviaQuest[i]);
+            ans = string.toLowerCase();
+            if (ans == triviaAns[i]) {
+                alert("Correct!");
+                attempts = 0;
+            } else {
+                alert("Incorrect");
+                attempts = attempts - 1;
+            }
+    
+            if (attempts == 2 && ans == triviaAns[i]) {
+                points = points + 3;
+            } else if (attempts == 1 && ans == triviaAns[i]) {
+                points = points + 2;
+            } else if (attempts == 0 && ans == triviaAns[i]) {
+                points = points + 1;
+            } else {
+                points = points + 0;
+                attempts == 0;
+            }
+            if (i === 4 && attempts == 0) {break; }
         }
-
-        if (attempts == 2 && ans == triviaAns[i]) {
-            points = points + 3;
-        } else if (attempts == 1 && ans == triviaAns[i]) {
-            points = points + 2;
-        } else if (attempts == 0 && ans == triviaAns[i]) {
-            points = points + 1;
-        } else {
-            points = points + 0;
-            attempts == 0;
-            
-        }
-
-        if (i === 3 && attempts == 0) {break; }
+      return points;
     }
-  return points;
 }
