0a11ec3897351ad48bb55ed8694ebc1139b2ae9e
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-11-03 22:30:41 -0600

    jscript and jquery update

---
+++ game.js
@@ -0,0 +1,20 @@
+var questions = [
+    ["What is the name of the dog that found the Jules Rimet Cup in 1966?", 0 [1], "Pickles", "Pudding", "Papaya"],
+    ["What is the name of the Soviet ghost town in Norway?", 1 [2], "Oslo", "Pryamiden", "Hamar"],
+    ["What is the second aria in the opera, Tosca?", 2 [3], "Vissi d'arte", "Ti soffoca il sangue", "E lucevan le stelle"]
+];
+
+var counter = 0;
+
+var start = document.getElementById('prompt').innerHTML = "<button onClick='playGame();' class='button'>Play Game</button>";
+
+function playGame() { 
+
+}
+
+function checkAnswer() {
+
+}
+
+// document.getElementById('idName').innerHTML;
+   // document.getElementById('idName').innerHTML = "New HTML";
