173777e576610ed3b439eed1c98c79db500b9456
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-11-23 15:28:31 -0600

    jquery dom test 2

--- jquery-dom/shopping.js
+++ jquery-dom/shopping.js
@@ -1,37 +1,35 @@
-
-
 $(document).ready(function() {
 
     var shoppingCart = 0;
 
-    var addHTML = "<span class='del'>Remove</span>";
-    $(".add").html(addHTML);
+    var addHTML = "<br><span class='del'>Remove</span>";
+    $('.add').append(addHTML);
 
     $('.add').click(function () {
-	var itemName = $(this).attr('name');
-	var itemID = $(this).attr('id');
-	shoppingCart++;
-	if (shoppingCart > 0) {
-		$('#empty').hide();
-	}
-	var cartLink = "<li class='cartItem' name='" + itemID + "'>" + itemName + " <span class='del'>Remove</span></li>";
-	$('#cart').append(cartLink);
-	$('#' + itemID).hide();
+		var itemName = $(this).attr('name');
+		var itemID = $(this).attr('id');
+		shoppingCart++;
+		if (shoppingCart > 0) {
+			$('#empty').hide();
+		}
+		var cartLink = "<li class='cartItem' name='" + itemID + "'>" + itemName + " <span class='del'>Remove</span></li>";
+		$('#cart').append(cartLink);
+		$('#' + itemID).hide();
 	});
 
-	$("li").on("click", ".del", function(){
-		$("li").parent().empty();
+	$('#cart').on('click', '.del', function(){
+		$('li').parent().empty();
 		shoppingCart--;
 		if (shoppingCart === 0) {
 			$('#empty').show();
 		}
-		$('#' + itemID).show();
+		$('.add').show();
 	});
 
-	$("#rating").click(function(){
-		$("img").siblings().attr("src", "staroff.gif");
-		$("img").closest().attr("src", "staron.gif");
-		$("img").prev().attr("src", "staron.gif");
+	$('#rating').click(function(){
+		$('img').siblings().attr('src', 'staroff.gif');
+		$('img').closest().attr('src', 'staron.gif');
+		$('img').prev().attr('src', 'staron.gif');
 	});
 
 });
