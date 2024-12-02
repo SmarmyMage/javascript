4f48f92f3938b2d51bd2ce6bb3bfff2a2449edfd
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-12-01 19:02:51 -0600

    jquery images hover update 2

--- jquery-images/gallery.js
+++ jquery-images/gallery.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-    $("#thumbs").hover(function(){
-         $(function(){
-            $("#thumbs").css("border-style: thin", "border-color: #006400", "box-shadow: 5px 10px");
-         });
-
-        $(function(){
-            $("#thumbs").css("border-style: none", "border-color: none", "box-shadow: none");
-        });
+    $("p").hover(function(){
+        $("#thumbs").css("border-style: thin", "border-color: #006400", "box-shadow: 5px 10px");
+    }, function(){
+        $("#thumbs").css("border-style: none", "border-color: none", "box-shadow: none");
     });
 });
