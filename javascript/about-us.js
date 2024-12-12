470b874582efa886a35fd69f8cef6086cf36e466
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2024-12-11 18:56:29 -0600

    finalize test 3

--- javascript/about-us.js
+++ javascript/about-us.js
@@ -1,3 +1,26 @@
-function colorClick(newColor) {
-    document.getElementById('color').setAttribute('href', newColor);
+window.onload = function extraHeading() {
+    document.getElementById('new').innerHTML = "<h2>Welcome to this page!</h2>";
 }
+
+$(document).ready(function() {
+    $("#hide").hide();
+
+    $(".show").click(function() {
+        $("#hide").show();
+        if ($('.show').text() === 'Show Grid') {
+            $('.show').text('Hide Grid');
+        } else {
+            $('.show').text('Show Grid');
+            $("#hide").hide();
+        }
+    })
+
+    $("#hover").hover(
+        function() {
+          $(this).attr("src", "Click_Icon2.jpg");
+        },
+        function() {
+          $(this).attr("src", "Click_Icon.jpg");
+        }
+      );
+})
